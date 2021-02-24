a15fa44743e2dc283f0b8aeb07e3360b34921c12
empower87 <mjamespower87@gmail.com>
2021-02-23 20:51:47 -0800

    uhh

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
+const movies = require('./movies')
 
 app.use(express.static('public'))
 
 app. get('/', (req, res) => {
-    res.sendFile(__)
+    res.sendFile(__dirname + '/views/home.html')
 })
-app.listen(3000, () => {
+app. get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+app. get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+})
+app. get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/views/contact.html')
+})
+app.get('/movies', (req, res) => {
+    console.log(req.query)// {title: 'The Godfather'}
+    let filteredMovies = movies.filter((each) => {
+    return (each.title.includes(req.query.title))
+    res.json({filteredMovies})
+})
+
+app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000')
 })
